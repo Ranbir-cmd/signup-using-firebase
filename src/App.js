893dfef9d3b4f2059c0f92b3d077792e27b3d477
Ranabir Mandal <mranbir07@gmail.com>
2022-05-21 10:50:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { app } from "./firebaseConfig";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 function App() {
+  const [data, setData] = useState({});
+  let auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+  const handleInput = (event) => {
+    // setting value in email and password at a same time, that is y using array
+    let newInput = { [event.target.name]: event.target.value };
+    setData({ ...data, ...newInput });
+  };
+
+  const handleSubmit = (event) => {
+    // TO SIGNUP
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then((response) => {
+        console.log(response.user);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+
+    // TO LOGIN
+    // signInWithEmailAndPassword(auth, data.email, data.password)
+    //   .then((response) => {
+    //     console.log(response.user);
+    //   })
+    //   .catch((err) => {
+    //     alert(err.message);
+    //   });
+  };
+
+  // SIGNUP WITH GOOGLE
+  const handleSignupGoogle = (event) => {
+    signInWithPopup(auth, googleProvider)
+      .then((response) => {
+        console.log(response.user);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        name="email"
+        type="email"
+        placeholder="Enter email"
+        onChange={(event) => handleInput(event)}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Enter password"
+        onChange={(event) => handleInput(event)}
+      />
+      <button onClick={handleSubmit}>Signup</button>
+      <button onClick={handleSignupGoogle}>Sign up with Google</button>
     </div>
   );
 }
---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYEkf2XXNCmH4FFMCvI2S6bKE6oBM56rw",
+  authDomain: "fir-intro2105.firebaseapp.com",
+  projectId: "fir-intro2105",
+  storageBucket: "fir-intro2105.appspot.com",
+  messagingSenderId: "314444207274",
+  appId: "1:314444207274:web:d391106a79496e3483acd3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
